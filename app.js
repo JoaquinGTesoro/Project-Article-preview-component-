370e11418fa4945b5f5d3c9b460fc3b3055e81a7
Joaquin G Tesoro <joaquingonzalez.tesoro@gmail.com>
2021-03-08 14:50:01 -0300

    finished mobile version

---
+++ app.js
@@ -0,0 +1,18 @@
+/*==================
+ Element Selectors
+==================*/
+
+const shareBtn = document.querySelectorAll(".share-btn-container");
+const shareSection = document.querySelector(".share-section");
+const reviewSection = document.querySelector(".review-section") 
+
+/*================
+ Event listeners
+================*/
+
+shareBtn.forEach(button => {
+    button.addEventListener('click', () => {
+        shareSection.classList.toggle("shift");
+        reviewSection.classList.toggle("shift");
+    });
+})
